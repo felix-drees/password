46974c262b024b9d0e74b3136138889d4b3f3d42
felix <name@email.nl>
2021-10-06 22:35:46 +0200

    added type and value validater

--- main.py
+++ main.py
@@ -9,6 +9,28 @@ from typing import Union
 DEFAULT_PW_CHARS: str = string.ascii_letters + string.digits + string.punctuation
 
 
+def is_valid_input(min_pw_len: int, max_pw_len: int, pw_chars: Union[str, list[str]]) -> bool:
+    """ validate the input for the below functions """
+
+    # CHECK TYPE
+
+    if not isinstance(min_pw_len and max_pw_len, int) or not hasattr(pw_chars, '__iter__'):
+        raise TypeError('invalid type for pw len or pw charset')
+
+    if False in [isinstance(char, str) for char in pw_chars]:
+        raise TypeError('pw charset dose NOT only contains characters')
+
+    # CHECK VALUE
+
+    if min_pw_len < 1 or len(pw_chars) < 1:
+        raise ValueError('invalid value for pw len or pw charset')
+
+    if min_pw_len > max_pw_len:
+        raise ValueError('min pw len is grater than max pw len')
+
+    return True
+
+
 def password_creator(pw_len: int = 40, pw_chars: Union[str, list[str]] = DEFAULT_PW_CHARS) -> str:
     """
     this function generates a password,
@@ -18,14 +40,13 @@ def password_creator(pw_len: int = 40, pw_chars: Union[str, list[str]] = DEFAULT
     :param pw_chars: the desired / allowed characters for creating the password
     :return: the created password
     """
-
     """ class secrets.SystemRandom
     A class for generating random numbers using the highest-quality sources provided by the operating system. 
     See random.SystemRandom for additional details.
     """
 
-    if not isinstance(pw_len, int) or not hasattr(pw_chars, '__iter__'):
-        raise TypeError('invalid type for pw len or pw charset')
+    if not is_valid_input(min_pw_len=pw_len, max_pw_len=pw_len, pw_chars=pw_chars):
+        raise SystemExit('invalid input')
 
     try:
         return ''.join(secrets.SystemRandom.choice(pw_chars) for _ in range(pw_len))
@@ -43,11 +64,9 @@ def generate_all_password_combinations(min_pw_len: int = 4, max_pw_len: int = 5,
     :param pw_chars: the desired / allowed characters for creating the password
     :return: all possible passwords
     """
-    if min_pw_len > max_pw_len:
-        raise ValueError('min pw len is grater than max pw len')
 
-    if not isinstance(min_pw_len and max_pw_len, int) or not hasattr(pw_chars, '__iter__'):
-        raise TypeError('invalid type for min pw len, max pw len or pw_chars')
+    if not is_valid_input(min_pw_len=min_pw_len, max_pw_len=max_pw_len, pw_chars=pw_chars):
+        raise SystemExit('invalid input')
 
     for index in range(min_pw_len, max_pw_len):
         for password in itertools.combinations_with_replacement(pw_chars, index):
@@ -63,11 +82,9 @@ def calculate_all_possible_password_combinations(min_pw_len: int, max_pw_len: in
     :param pw_chars: the desired / allowed characters for creating the password
     :return: the power of the key space
     """
-    if min_pw_len > max_pw_len:
-        raise ValueError('min pw len is grater than max pw len')
 
-    if not isinstance(min_pw_len and max_pw_len, int) or not hasattr(pw_chars, '__iter__'):
-        raise TypeError('invalid type for min pw len, max pw len or pw_chars')
+    if not is_valid_input(min_pw_len=min_pw_len, max_pw_len=max_pw_len, pw_chars=pw_chars):
+        raise SystemExit('invalid input')
 
     return sum([pow(len(pw_chars), k) for k in range(min_pw_len, max_pw_len)])
 
